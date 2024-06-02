8f5f2188915cc913a5e005b1af133d43d8ff8781
Tatiana Smoliar <43788903+Tabonita@users.noreply.github.com>
2024-06-02 12:20:48 +0200

    Bubble sort (#6)

---
+++ include/bubble/bubble_sort.hpp
@@ -0,0 +1,6 @@
+#include <iostream>
+#include <vector>
+#include <iterator>
+
+void bubbleSort(std::vector<int>::iterator begin, std::vector<int>::iterator end);
+
--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,5 @@
 #include <argparse/argparse.hpp>
 #include <iostream>
-#include <my_fibonacci/sequence.hpp>
 
 int main(int argc, char** argv) {
   argparse::ArgumentParser program("generate_sequence");
@@ -16,11 +15,6 @@ int main(int argc, char** argv) {
     return 1;
   }
 
-  const auto n = program.get<int>("n");
-  const auto sequence = my_fibonacci::fibonacci_sequence(n);
-  for (auto val : sequence) {
-    std::cout << val << " ";
-  }
   std::cout << std::endl;
 
   return 0;
